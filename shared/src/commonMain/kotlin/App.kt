11281328a965ee65a2b160020708b3b84f3595d1
Abdul Basit <abdulbasit5361234@gmail.com>
2023-04-21 18:23:24 +0500

    Add stars background

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,4 +1,10 @@
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -13,24 +19,29 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 @Composable
 fun App() {
     MaterialTheme {
         Box(
-            Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            Modifier.fillMaxSize(), contentAlignment = Alignment.Center
         ) {
+            StarsBackground()
             PuzzleGame()
         }
     }
@@ -76,9 +87,7 @@ fun PuzzleGame() {
     }
 
     Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
         LazyColumn {
             items(3) { row ->
@@ -87,21 +96,27 @@ fun PuzzleGame() {
                         val position = row * 3 + col
                         val tile = tilePositions[position]
                         val isTileInRightPos = isTileInRightPosition(position)
-                        Box(
-                            Modifier
-                                .padding(4.dp)
-                                .border(border = BorderStroke(2.dp, color = Color.DarkGray))
-                                .size(100.dp)
-                                .background(
-                                    color = if (isTileInRightPos) Color.Green else Color.White,
-                                    shape = RoundedCornerShape(8.dp),
-                                )
-                                .clickable {
-                                    moveTile(position)
-                                }) {
+                        Box(Modifier.padding(4.dp)
+                            .border(
+                                border = BorderStroke(
+                                    2.dp,
+                                    color = Color.White.copy(alpha = 0.6f),
+                                ),
+                                shape = RoundedCornerShape(5.dp)
+                            )
+                            .size(100.dp).background(
+                                color = if (isTileInRightPos) Color.Gray.copy(0.5f) else Color.White.copy(
+                                    alpha = 0.1f
+                                ),
+                                shape = RoundedCornerShape(8.dp),
+                            )
+                            .clickable {
+                                moveTile(position)
+                            }) {
                             if (tile != 0) {
                                 Text(
-                                    tile.toString(),
+                                    text = tile.toString(),
+                                    color = Color.White,
                                     fontSize = 24.sp,
                                     fontWeight = FontWeight.Bold,
                                     textAlign = TextAlign.Center,
@@ -119,6 +134,68 @@ fun PuzzleGame() {
         shuffleTiles()
     }
 }
+
+@Composable
+fun StarsBackground() {
+    val stars = remember { mutableStateListOf<Star>() }
+
+    LaunchedEffect(true) {
+        // Add stars to the list
+        repeat(200) {
+            stars.add(
+                Star(
+                    x = Random.nextFloat(),
+                    y = Random.nextFloat(),
+                    radius = Random.nextFloat().coerceIn(3f, 10f),
+                    brightness = Random.nextFloat().coerceIn(0.2f, 1f)
+                )
+            )
+        }
+    }
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val width = size.width
+        val height = size.height
+
+        // Draw a background color gradient
+        drawRect(
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    Color(0xFF202840),
+                    Color(0xFF0A0E17),
+                    Color(0xFF02030A),
+                ), startY = 0f, endY = height
+            ), size = Size(width, height)
+        )
+    }
+
+    // Draw the stars
+    stars.forEach { star ->
+        val infiniteTransition = rememberInfiniteTransition()
+        val brightness by infiniteTransition.animateFloat(
+            initialValue = star.brightness,
+            targetValue = 1f - star.brightness,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = Random.nextInt(1000, 3000)),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val width = size.width
+            val height = size.height
+            drawCircle(
+                color = Color.White.copy(alpha = brightness),
+                radius = star.radius,
+                center = Offset(star.x * width, star.y * height)
+            )
+        }
+    }
+}
+
+data class Star(
+    var x: Float, var y: Float, var radius: Float, var brightness: Float
+)
 
 
 fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
