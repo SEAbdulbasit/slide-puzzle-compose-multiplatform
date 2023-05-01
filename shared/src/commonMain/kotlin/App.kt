f2be3bfc52e78ad6b719fe3d18689b49ebe515bc
Abdul Basit <abdulbasit5361234@gmail.com>
2023-05-01 10:11:33 +0500

    Add 4x4, 5x5 and 6x6 matrix

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,345 +1,150 @@
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlin.random.Random
 
 @Composable
 fun App() {
     MaterialTheme {
+        val isSideMenuOpen = remember { mutableStateOf(false) }
+        val selectedPuzzleSize = remember { mutableStateOf(3) }
         Box(
-            Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            Modifier.fillMaxSize()
         ) {
-            StarsBackground()
-            MeteoroidAnimation(Modifier)
-            PuzzleGame()
-        }
-    }
-}
-
-
-@Composable
-fun PuzzleGame() {
-    val tilePositions = remember { mutableStateListOf(1, 2, 3, 4, 5, 6, 7, 8, 0) }
-    val emptyTilePosition = remember { mutableStateOf(8) }
-
-    fun shuffleTiles() {
-        // Shuffle the tiles using Fisher-Yates shuffle algorithm
-        for (i in 8 downTo 1) {
-            val j = (0..i).random()
-            tilePositions.swap(i, j)
-        }
-        emptyTilePosition.value = tilePositions.indexOf(0)
-    }
-
-    fun canMoveTile(position: Int): Boolean {
-        val emptyPosition = emptyTilePosition.value
-        return (position - emptyPosition == 1 && position % 3 != 0) || (emptyPosition - position == 1 && emptyPosition % 3 != 0) || position - emptyPosition == 3 || position - emptyPosition == -3
-    }
-
-    fun isPuzzleSolved(): Boolean {
-        return tilePositions == listOf(1, 2, 3, 4, 5, 6, 7, 8, 0)
-    }
-
-    fun isTileInRightPosition(position: Int): Boolean {
-        return tilePositions[position] == position + 1
-    }
-
-    fun moveTile(position: Int) {
-        if (canMoveTile(position)) {
-            val emptyPosition = emptyTilePosition.value
-            tilePositions.swap(emptyPosition, position)
-            emptyTilePosition.value = position
-
-            if (isPuzzleSolved()) {
+            PuzzleBoard(selectedPuzzleSize.value)
+            Box(
+                Modifier.padding(16.dp).border(
+                    border = BorderStroke(
+                        2.dp,
+                        color = Color.White.copy(alpha = 0.6f),
+                    ), shape = RoundedCornerShape(16.dp)
+                )
+            ) {
+                Icon(imageVector = Icons.Default.Menu,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.padding(16.dp).size(24.dp).clickable {
+                        isSideMenuOpen.value = true
+                    })
             }
-        }
-    }
+            if (isSideMenuOpen.value) {
+                Box(
+                    modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.2f))
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(top = 32.dp, bottom = 32.dp, end = 32.dp).border(
+                            shape = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp), border = BorderStroke(
+                                width = 2.dp,
+                                color = Color.White.copy(alpha = 0.6f),
+                            )
+                        ).background(
+                            color = Color(0xFF202840).copy(alpha = 0.9f),
+                            shape = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp)
+                        ).clipToBounds()
 
-    Box(
-        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-    ) {
-        LazyColumn {
-            items(3) { row ->
-                LazyRow {
-                    items(3) { col ->
-                        val position = row * 3 + col
-                        val tile = tilePositions[position]
-                        val isTileInRightPos = isTileInRightPosition(position)
-                        Box(Modifier.padding(4.dp)
-                            .border(
-                                border = BorderStroke(
-                                    2.dp,
-                                    color = Color.White.copy(alpha = 0.6f),
-                                ),
-                                shape = RoundedCornerShape(16.dp)
-                            )
-                            .size(100.dp).background(
-                                color = if (isTileInRightPos) Color.Gray.copy(0.5f) else Color.White.copy(
-                                    alpha = 0.1f
-                                ),
-                                shape = RoundedCornerShape(16.dp),
-                            )
-                            .clickable {
-                                moveTile(position)
-                            }) {
-                            if (tile != 0) {
+                    ) {
+
+                        Column {
+                            Box(modifier = Modifier.fillMaxWidth()) {
                                 Text(
-                                    text = tile.toString(),
-                                    color = Color.White,
-                                    fontSize = 24.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier.align(Alignment.Center)
+                                    text = "Slide Puzzle",
+                                    style = MaterialTheme.typography.h5.copy(Color.White),
+                                    modifier = Modifier.padding(16.dp).align(Alignment.TopStart)
+
+                                )
+                                Icon(
+                                    imageVector = Icons.Default.Create,
+                                    contentDescription = null,
+                                    tint = Color.White,
+                                    modifier = Modifier.padding(top = 18.dp, end = 16.dp).size(24.dp).clickable {
+                                        isSideMenuOpen.value = false
+                                    }.align(Alignment.TopEnd)
                                 )
                             }
+                            Text(
+                                text = "Puzzle Size",
+                                style = MaterialTheme.typography.body1.copy(color = Color.White.copy(alpha = 0.8f)),
+                                modifier = Modifier.padding(
+                                    top = 16.dp, start = 16.dp
+                                )
+                            )
+                            Text(
+                                text = "Select the size of your puzzle",
+                                style = MaterialTheme.typography.body1.copy(color = Color.White.copy(alpha = 0.8f)),
+                                modifier = Modifier.padding(
+                                    top = 16.dp, start = 16.dp
+                                )
+                            )
+                            PuzzleSizeBoxOptions(selectedPuzzleSize = selectedPuzzleSize.value, onPuzzleSizeChanged = {
+                                selectedPuzzleSize.value = it
+                            })
                         }
                     }
                 }
             }
         }
     }
+}
 
-    LaunchedEffect(Unit) {
-        shuffleTiles()
+@Composable
+fun PuzzleSizeBoxOptions(onPuzzleSizeChanged: (Int) -> Unit, selectedPuzzleSize: Int) {
+    val puzzleSizes = listOf(3, 4, 5, 6)
+    LazyRow(modifier = Modifier.fillMaxWidth().padding(top = 16.dp, start = 16.dp, end = 16.dp).clipToBounds()) {
+        items(items = puzzleSizes, itemContent = {
+            PuzzleSizeBox(it, selectedPuzzleSize, onPuzzleSizeChanged)
+        })
     }
 }
 
 @Composable
-fun StarsBackground() {
-    val stars = remember { mutableStateListOf<Star>() }
-
-    LaunchedEffect(true) {
-        // Add stars to the list
-        repeat(200) {
-            stars.add(
-                Star(
-                    x = Random.nextFloat(),
-                    y = Random.nextFloat(),
-                    radius = Random.nextFloat().coerceIn(3f, 10f),
-                    brightness = Random.nextFloat().coerceIn(0.2f, 1f)
-                )
+fun PuzzleSizeBox(puzzleSize: Int, selectedPuzzleSize: Int, onPuzzleSizeChanged: (Int) -> Unit) {
+    Column {
+        Box(Modifier.padding(end = 8.dp).border(
+            border = BorderStroke(
+                2.dp,
+                color = Color.White.copy(alpha = 0.6f),
+            ), shape = RoundedCornerShape(8.dp)
+        ).background(
+            color = if (puzzleSize == selectedPuzzleSize) Color.White else Color(0xFF202840),
+            shape = RoundedCornerShape(8.dp),
+        ).clickable {
+            onPuzzleSizeChanged(puzzleSize)
+        }) {
+            Text(
+                text = "$puzzleSize x $puzzleSize",
+                color = if (puzzleSize == selectedPuzzleSize) Color(0xFF202840) else Color.White,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center).padding(16.dp)
             )
         }
-    }
-
-    val infiniteTransition = rememberInfiniteTransition()
-
-    // Add moon to the list
-    val moon = remember {
-        Star(
-            x = 0f,
-            y = 0.4f,
-            radius = 80f,
-            brightness = 1f
-        )
-    }
-
-    // Animate moon position
-    val moonPosition by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 1f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(15000, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-
-    moon.x = moonPosition
-
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val width = size.width
-        val height = size.height
-
-        // Draw a background color gradient
-        drawRect(
-            brush = Brush.verticalGradient(
-                colors = listOf(
-                    Color(0xFF202840),
-                    Color(0xFF0A0E17),
-                    Color(0xFF02030A),
-                ), startY = 0f, endY = height
-            ), size = Size(width, height)
-        )
-    }
-
-    // Draw the stars
-    stars.forEach { star ->
-        val brightness by infiniteTransition.animateFloat(
-            initialValue = star.brightness,
-            targetValue = 1f - star.brightness,
-            animationSpec = infiniteRepeatable(
-                animation = tween(durationMillis = Random.nextInt(1000, 3000)),
-                repeatMode = RepeatMode.Reverse
-            )
-        )
-
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            val width = size.width
-            val height = size.height
-            drawCircle(
-                color = Color.White.copy(alpha = brightness),
-                radius = star.radius,
-                center = Offset(star.x * width, star.y * height)
-            )
-        }
-    }
-
-    // Draw the moon
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        drawCircle(
+        Text(
+            text = "${puzzleSize * puzzleSize - 1} tiles",
             color = Color.White,
-            radius = moon.radius,
-            center = Offset(moon.x * this.size.width, moon.radius * 4)
+            style = MaterialTheme.typography.caption,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 8.dp)
         )
     }
-}
-
-@Composable
-fun MeteoroidAnimation(modifier: Modifier) {
-    val meteoroids = remember { mutableStateListOf<Meteoroid>() }
-
-    LaunchedEffect(true) {
-        // Add meteoroids to the list
-        repeat(20) {
-            meteoroids.add(
-                Meteoroid(
-                    x = Random.nextFloat(),
-                    y = Random.nextFloat(),
-                    size = Random.nextFloat().coerceIn(10f, 20f),
-                    speed = Random.nextInt(10, 30).toFloat()
-                )
-            )
-        }
-    }
-
-    Canvas(modifier = modifier.fillMaxSize()) {
-        val width = size.width
-        val height = size.height
-
-        // Draw the meteoroids
-        meteoroids.forEach { meteoroid ->
-            // Move the meteoroid
-            meteoroid.x += meteoroid.speed / 1000f
-
-            // If the meteoroid is off-screen, reset it
-            if (meteoroid.x > 1f) {
-                meteoroid.x = -meteoroid.size
-                meteoroid.y = Random.nextFloat()
-                meteoroid.speed = Random.nextInt(10, 30).toFloat()
-            }
-
-            // Draw the meteoroid
-            drawCircle(
-                color = Color.Gray,
-                radius = meteoroid.size,
-                center = Offset(meteoroid.x * width, meteoroid.y * height)
-            )
-        }
-    }
-}
-
-data class Meteoroid(
-    var x: Float,
-    var y: Float,
-    val size: Float,
-    var speed: Float
-)
-
-
-@Composable
-fun FlameAnimation(
-    modifier: Modifier = Modifier,
-    durationMillis: Int = 1000,
-    color: Color = Color.Yellow
-) {
-    val animProgress = remember { Animatable(0f) }
-
-    LaunchedEffect(Unit) {
-        animProgress.animateTo(
-            targetValue = 1f,
-            animationSpec = infiniteRepeatable(
-                animation = tween(durationMillis = durationMillis),
-                repeatMode = RepeatMode.Reverse
-            )
-        )
-    }
-
-    val path = remember {
-        Path().apply {
-            moveTo(0f, 0f)
-            lineTo(20f, 40f)
-            lineTo(10f, 30f)
-            lineTo(30f, 70f)
-            lineTo(20f, 60f)
-            lineTo(40f, 100f)
-            lineTo(0f, 60f)
-            lineTo(-20f, 100f)
-            lineTo(0f, 50f)
-        }
-    }
-
-    Canvas(modifier = modifier) {
-        val colorAlpha = (1f - animProgress.value) * 0.5f
-        val brush = Brush.linearGradient(
-            colors = listOf(
-                color.copy(alpha = colorAlpha),
-                color.copy(alpha = 0f)
-            )
-        )
-        drawPath(path, brush)
-    }
-}
-
-
-data class Star(
-    var x: Float, var y: Float, var radius: Float, var brightness: Float
-)
-
-data class Moon(
-    var x: Float,
-    var y: Float,
-    val radius: Float,
-    val color: Color,
-)
-
-
-fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
-    val temp = this[index1]
-    this[index1] = this[index2]
-    this[index2] = temp
 }
---
+++ shared/src/commonMain/kotlin/GameMainScreen.kt
@@ -0,0 +1,273 @@
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.random.Random
+
+/**
+ * Created by abdulbasit on 30/04/2023.
+ */
+
+@Composable
+fun PuzzleBoard(value: Int) {
+    val boxWidth = remember { mutableStateOf(-1f) }
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        StarsBackground { boxWidth.value = it - 16 }
+        MeteoroidAnimation(Modifier)
+        PuzzleTiles(value, boxWidth.value)
+    }
+}
+
+@Composable
+fun PuzzleTiles(puzzleSize: Int, boxWidth: Float) {
+    val numTiles = puzzleSize * puzzleSize
+    val tilePositions =
+        remember(key1 = puzzleSize) { mutableStateListOf(*(1 until numTiles).toList().toTypedArray(), 0) }
+    val emptyTilePosition = remember { mutableStateOf(numTiles - 1) }
+
+    fun shuffleTiles() {
+
+        // Shuffle the tiles using Fisher-Yates shuffle algorithm
+        for (i in numTiles - 1 downTo 1) {
+            val j = (0..i).random()
+            tilePositions.swap(i, j)
+        }
+        emptyTilePosition.value = tilePositions.indexOf(0)
+    }
+
+    LaunchedEffect(puzzleSize) {
+        shuffleTiles()
+    }
+
+    fun canMoveTile(position: Int): Boolean {
+        val emptyPosition = emptyTilePosition.value
+        val row = position / puzzleSize
+        val col = position % puzzleSize
+        val emptyRow = emptyPosition / puzzleSize
+        val emptyCol = emptyPosition % puzzleSize
+        return (row == emptyRow && col == emptyCol - 1) || // Left
+                (row == emptyRow && col == emptyCol + 1) || // Right
+                (row == emptyRow - 1 && col == emptyCol) || // Up
+                (row == emptyRow + 1 && col == emptyCol)    // Down
+    }
+
+    fun isPuzzleSolved(): Boolean {
+        return tilePositions == (1 until numTiles).toList() + listOf(0)
+    }
+
+    fun isTileInRightPosition(position: Int): Boolean {
+        return tilePositions[position] == position + 1
+    }
+
+    fun moveTile(position: Int) {
+        if (canMoveTile(position)) {
+            val emptyPosition = emptyTilePosition.value
+            tilePositions.swap(emptyPosition, position)
+            emptyTilePosition.value = position
+
+            if (isPuzzleSolved()) {
+            }
+        }
+    }
+
+    val tileSize = remember(boxWidth) { boxWidth / puzzleSize }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        LazyColumn {
+            items(puzzleSize) { row ->
+                LazyRow {
+                    items(puzzleSize) { col ->
+                        val position = row * puzzleSize + col
+                        val tile = tilePositions[position]
+                        val isTileInRightPos = isTileInRightPosition(position)
+                        Box(Modifier.padding(4.dp).border(
+                            border = BorderStroke(
+                                width = 2.dp,
+                                color = Color.White.copy(alpha = 0.6f),
+                            ), shape = RoundedCornerShape(16.dp)
+                        ).width((tileSize / puzzleSize).dp).aspectRatio(1f).background(
+                            color = if (isTileInRightPos) Color.Gray.copy(0.5f) else Color.White.copy(
+                                alpha = 0.1f
+                            ),
+                            shape = RoundedCornerShape(16.dp),
+                        ).clickable {
+                            moveTile(position)
+                        }) {
+                            if (tile != 0) {
+                                Text(
+                                    text = tile.toString(),
+                                    color = Color.White,
+                                    fontSize = 24.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        shuffleTiles()
+    }
+}
+
+
+@Composable
+fun StarsBackground(updateBoxWidth: (Float) -> Unit) {
+    val stars = remember { mutableStateListOf<Star>() }
+
+    LaunchedEffect(true) {
+
+        // Add stars to the list
+        repeat(200) {
+            stars.add(
+                Star(
+                    x = Random.nextFloat(),
+                    y = Random.nextFloat(),
+                    radius = Random.nextFloat().coerceIn(3f, 10f),
+                    brightness = Random.nextFloat().coerceIn(0.1f, 0.3f)
+                )
+            )
+        }
+    }
+
+    val infiniteTransition = rememberInfiniteTransition()
+
+    // Add moon to the list
+    val moon = remember {
+        Star(
+            x = 0f, y = 0.4f, radius = 80f, brightness = 1f
+        )
+    }
+
+    // Animate moon position
+    val moonPosition by infiniteTransition.animateFloat(
+        initialValue = 0f, targetValue = 1f, animationSpec = infiniteRepeatable(
+            animation = tween(15000, easing = LinearEasing), repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    moon.x = moonPosition
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val width = size.width
+        val height = size.height
+
+        // Draw a background color gradient
+        drawRect(
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    Color(0xFF202840),
+                    Color(0xFF0A0E17),
+                    Color(0xFF02030A),
+                ), startY = 0f, endY = height
+            ), size = Size(width, height)
+        )
+    }
+
+    // Draw the stars
+    stars.forEach { star ->
+        val brightness by infiniteTransition.animateFloat(
+            initialValue = star.brightness, targetValue = 1f - star.brightness, animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = Random.nextInt(1000, 2000)), repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val width = size.width
+            val height = size.height
+            drawCircle(
+                color = Color.White.copy(alpha = brightness),
+                radius = star.radius,
+                center = Offset(star.x * width, star.y * height)
+            )
+        }
+    }
+
+    // Draw the moon
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        updateBoxWidth(this.size.width)
+        drawCircle(
+            color = Color.White, radius = moon.radius, center = Offset(moon.x * this.size.width, moon.radius * 4)
+        )
+    }
+}
+
+@Composable
+fun MeteoroidAnimation(modifier: Modifier) {
+    val meteoroids = remember { mutableStateListOf<Meteoroid>() }
+
+    LaunchedEffect(true) {
+        // Add meteoroids to the list
+        repeat(20) {
+            meteoroids.add(
+                Meteoroid(
+                    x = Random.nextFloat(),
+                    y = Random.nextFloat(),
+                    size = Random.nextFloat().coerceIn(10f, 20f),
+                    speed = Random.nextInt(10, 30).toFloat()
+                )
+            )
+        }
+    }
+
+    Canvas(modifier = modifier.fillMaxSize()) {
+        val width = size.width
+        val height = size.height
+
+
+        // Draw the meteoroids
+        meteoroids.forEach { meteoroid ->
+            // Move the meteoroid
+            meteoroid.x += meteoroid.speed / 1000f
+
+            // If the meteoroid is off-screen, reset it
+            if (meteoroid.x > 1f) {
+                meteoroid.x = -meteoroid.size
+                meteoroid.y = Random.nextFloat()
+                meteoroid.speed = Random.nextInt(10, 30).toFloat()
+            }
+
+            // Draw the meteoroid
+            drawCircle(
+                color = Color.Gray, radius = meteoroid.size, center = Offset(meteoroid.x * width, meteoroid.y * height)
+            )
+        }
+    }
+}
+
+data class Meteoroid(
+    var x: Float, var y: Float, val size: Float, var speed: Float
+)
+
+data class Star(
+    var x: Float, var y: Float, var radius: Float, var brightness: Float
+)
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
+}
